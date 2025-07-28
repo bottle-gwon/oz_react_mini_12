c7518eea543a1561c9f93bc856ec30473bcd74e7
bottle-gwon <lazyguy1@daum.net>
2025-07-28 16:58:40 +0900

    feat add search page exception handling

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
 import { useSearchParams } from "react-router-dom"
 import { MovieCard } from "../components/MovieCard";
+import SkeletonSlide from "../skeleton/SkeletonSlide";
 
 export default function Search() {
   const [searchParams] = useSearchParams()
   const [result, setResult] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [searchError, setSearchError] = useState(false);
   const options = {
     method: 'GET',
     headers: {
@@ -18,16 +20,20 @@ export default function Search() {
     const fetchSearch = async () =>{
       try{
         const encodeSearch = encodeURIComponent(searchParams.get('movie'));
-
+        //search/multi?query를 쓰면 배우, 회사, 영화 동시에 검색가능하다. 이후에 상황에 따라 변경 할것
         const searchResponse = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeSearch}&include_adult=false&language=ko-KR&page=1` , options);
         if(!searchResponse.ok){
           console.log(searchResponse)
           throw new Error(`search loading fail`)
         }
+          console.log(searchResponse)
         const data  = await searchResponse.json();
 
         setResult(data.results)
+          console.log(result)
+
       }catch(e){
+        setSearchError(true);
         console.error(e);
       }finally{
         setIsLoading(false)
@@ -39,14 +45,15 @@ export default function Search() {
 
   return(
     <>
-      {/* {console.log(result)} */}
-      {/* {searchParams.get('movie')} */}
       <div className="flex flex-wrap bg-black">
-        {result.map((el)=>
+        {
+        searchError? <div>오류 발생</div>:
+        isLoading? <SkeletonSlide /> : result.map((el)=>
         <div className="w-1/4">
           <MovieCard movie={el}/>
         </div>
-        )}
+        )  }
+        {result.length ===0 ? <div>검색 결과 없음</div> : null }
       </div>
     </>
   )
